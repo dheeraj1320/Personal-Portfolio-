1d57e0c66b5f362271d8ca7ef9f80dd32ed76ec2
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2022-12-15 15:00:31 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 "strict mode";
 
+// Variables
+const submitBtn = document.querySelector(".submitBtn");
+
+// Functions
+const submitHandler = () => {
+  console.log("djdjdjdjjd");
+};
+
+// Event Handlers
+submitBtn.addEventListener("click", submitHandler);
+
+// Slider
+
 $(".carousel").carousel({
   interval: 3000,
   keyboard: true,
